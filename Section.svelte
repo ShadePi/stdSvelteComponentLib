65911f9168cfc4f059878eab6b76efe6abe0c8fe
ShadePi <erikkirkegaardstage@gmail.com>
2021-05-21 16:58:49 +0200

    Section

---
+++ Section.svelte
@@ -0,0 +1,19 @@
+<script>
+    export const Yes;
+
+    export const Title;
+
+    export const Text;
+</script>
+
+<style>
+    h1, p {
+        text-align: center;
+        font-family: 'Times New Roman';
+    }
+</style>
+
+<div>
+    <h1>{ Title }</h1>
+    <p>{ Text }</p>
+</div>
